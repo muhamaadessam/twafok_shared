830c2f0822cb64e1632e05f8a1da3e3426a43b35
muhammadessam <muhammad159e@gmail.com>
2026-05-15 16:41:51 +0300

    Add export for DioConfig in twafok_shared.dart

--- lib/twafok_shared.dart
+++ lib/twafok_shared.dart
@@ -8,5 +8,6 @@ export 'core/core.dart';
 export 'config/twafok_config.dart';
 export 'package:dartz/dartz.dart';
 export 'package:equatable/equatable.dart';
+export 'config/dio_config.dart';
 
 // TODO: Export any libraries intended for clients of this package.
