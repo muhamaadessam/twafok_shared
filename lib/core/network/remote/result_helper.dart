b983e8495176775d773590a0493a2e41842e7cbb
muhammadessam <muhammad159e@gmail.com>
2026-05-16 18:47:32 +0300

    Refactor fold method in Result class for improved readability and parameter order

--- lib/core/network/remote/result_helper.dart
+++ lib/core/network/remote/result_helper.dart
@@ -3,21 +3,24 @@ import '../../error/failure.dart';
 sealed class Result<T> {
   const Result();
 
-  R fold<R>({
-    required R Function(T data) onSuccess,
-    required R Function(Failure failure) onError,
-  })=> switch(this){
-    Success(:final data) => onSuccess(data),
-    Error(:final failure) => onError(failure),
-  };
+  R fold<R>(
+    R Function(Failure failure) onError,
+    R Function(T data) onSuccess,
+  ) =>
+      switch (this) {
+        Success(:final data) => onSuccess(data),
+        Error(:final failure) => onError(failure),
+      };
 }
 
 final class Success<T> extends Result<T> {
   final T data;
+
   const Success(this.data);
 }
 
 final class Error<T> extends Result<T> {
   final Failure failure;
+
   const Error(this.failure);
 }
